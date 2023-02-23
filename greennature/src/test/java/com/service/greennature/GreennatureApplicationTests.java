45fe6747c7f41acdda75acb7b9a1b07fd8a512a0
Matheus Fillipe toyc497 <matheusfillipe497@gmail.com>
2023-02-23 13:49:39 -0300

    estrutura do projeto

---
+++ greennature/src/test/java/com/service/greennature/GreennatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.greennature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreennatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
