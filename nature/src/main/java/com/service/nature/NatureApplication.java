c74739863ebdb8af5541b3aa70ddd595e7a39e4a
Matheus Fillipe toyc497 <matheusfillipe497@gmail.com>
2023-02-24 13:04:45 -0300

    Subindo estrutura do projeto springboot

---
+++ nature/src/main/java/com/service/nature/NatureApplication.java
@@ -0,0 +1,13 @@
+package com.service.nature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatureApplication.class, args);
+	}
+
+}
---
+++ nature/src/test/java/com/service/nature/NatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.nature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
